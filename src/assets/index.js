35aa5b87203b85f9202190b1b3b691b44703ce51
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2025-02-26 12:54:52 +0530

    minor

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import Logo from "./logo.png.svg"
 import HeroImage from "./hero_section.png"
-import StepsImage from "./steps_image.svg"
+import StepsImage from "./Steps_image.svg"
 import ScholarIcon from "./scholar.svg"
 import CTA from "./cta.png"
 import HeroMobile from "./Hero_mobile.png"
